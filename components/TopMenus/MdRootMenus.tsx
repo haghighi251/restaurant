1bd3dbd1e46f05bb65a1085150487006aa61a5cf
amir <haghighi251@gmail.com>
2023-01-10 22:08:04 +0330

    TopMenu update

--- components/TopMenus/MdRootMenus.tsx
+++ components/TopMenus/MdRootMenus.tsx
@@ -8,30 +8,26 @@ const MdRootMenus: FC<MdRootMenusProps> = ({items}: MdRootMenusProps): JSX.Eleme
         return <></>
 
     return (<div className='flex gap-x-1 text-white font-bold text-lg justify-center items-center'>
-                {items && items.map((item: TopMenuRoot, index: number) => {
-                    return <div className="group/item relative" key={index}>
-                        <Link href={item.link}
-                              className={`grid grid-cols-2 gap-x-0.5  font-boldItalic hover:text-amber-600 cursor-pointer items-center`}>
-                            {item.name}
-                            {item.hasSubitem !== undefined && <label className="md:px-1 lg:px-0.5 mr-1 ml-2 md:ml-0 pt-1 cursor-pointer">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
-                                     stroke="currentColor" className="w-6 h-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5"/>
-                                </svg>
-                            </label>}
-                        </Link>
-                        {/* Sub menu items */}
-<<<<<<< HEAD
-                        {item.hasSubitem !== undefined && item.subitems !== undefined && <div className="hidden group-hover/item:block" id={`${item.name}SubItems`}>
-=======
+        {items && items.map((item: TopMenuRoot, index: number) => {
+            return <div className="group/item relative" key={index}>
+                <Link href={item.link}
+                      className={`grid grid-cols-2 gap-x-0.5  font-boldItalic hover:text-amber-600 cursor-pointer items-center`}>
+                    {item.name}
+                    {item.hasSubitem !== undefined && <label className="md:px-1 lg:px-0.5 mr-1 ml-2 md:ml-0 pt-1 cursor-pointer">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                             stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5"/>
+                        </svg>
+                    </label>}
+                </Link>
+                {/* Sub menu items */}
                         {item.hasSubitem !== undefined && item.subitems !== undefined && <div className="hidden group-hover/item:block z-10" id={`${item.name}SubItems`}>
->>>>>>> development
                             <MdSubMenuItem items={item.subitems} />
                         </div> }
                     </div>
-                })}
+                    })}
             </div>
-    );
-};
+        );
+        };
 
 export default MdRootMenus;
